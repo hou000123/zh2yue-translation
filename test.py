fefaafc4c847095c3075a8756d99b6b706657a8d
hou000123 <hou000123@gmail.com>
2026-03-08 13:37:42 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+
+model_name = "hou000123/zh2yue-translation"  
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+def translate_to_cantonese(text):
+    inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True)
+    outputs = model.generate(**inputs)
+    return tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+print(translate_to_cantonese("今天天气很好"))  # 输出: "今日天氣好好"
